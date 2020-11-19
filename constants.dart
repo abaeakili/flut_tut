099a6dd9191cdee8e4885590ae1275703cd1ba36
Abae Akili <mohaabae@yahoo.com>
2020-11-19 08:42:52 +0300

    Lesson 3: ◉ Constants

---
+++ constants.dart
@@ -0,0 +1,11 @@
+// You can change values of Variables but not constants
+
+void main() {
+  const name2 = "Zahra";
+
+  print(name2);
+
+  // name2 = "Omar";
+}
+// Constant variables can't be assigned a value.
+// Try removing the assignment, or remove the modifier 'const' from the variable
