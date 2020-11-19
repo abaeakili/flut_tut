9d150a12b327b44ceea2bbe96e9c34a21dc2770f
Abae Akili <mohaabae@yahoo.com>
2020-11-19 08:36:52 +0300

    🪁 Added Variables

---
+++ variables.dart
@@ -0,0 +1,19 @@
+/*
+Number : int
+Strings : str
+Boolean : bool
+*/
+
+void main() {
+  int x = 23;
+  String name = "Akili";
+  double number = 2.2;
+  bool student = true;
+  var male = 23;
+
+  print(x);
+  print(name);
+  print(number);
+  print(student);
+  print(male);
+}
