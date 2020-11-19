4f9654a94df0ca8a33ee4370cfe1ffa3e3716c66
Abae Akili <mohaabae@yahoo.com>
2020-11-19 10:13:31 +0300

    Lesson 7: ⩸ Adding items; int, String & dynamic

---
+++ lists.dart
@@ -0,0 +1,14 @@
+// These can hold items, variables etc
+
+void main() {
+  List<int> myList = [4, 5, 7];
+  
+  print(myList);
+  print(myList[0]);
+
+  List<String> names = ["Zahra", "Aboud", "Fatma"];
+  print(names[1]);
+
+  List<dynamic> differentTypes = [45, "Fatma", true];
+  print(differentTypes[2]);
+}
