20e3b894834d8799727d84511c8bb7b1973334e3
Abae Akili <mohaabae@yahoo.com>
2020-11-19 08:29:06 +0300

    🏃🏽‍♀️ Running a dart file.

--- main.dart
+++ main.dart
@@ -1,7 +1,13 @@
-main() {
-  for (var i = 1; i <= 10; i++) {
-    for (var j = 1; j <= 10; j++) {
-      print("$i*$j=${i * j}");
-    }
-  }
+void main() {
+  print("Hi!");
 }
+
+// Access the dart pad at dartpat.dartlang.org
+
+// main() {
+//   for (var i = 1; i <= 10; i++) {
+//     for (var j = 1; j <= 10; j++) {
+//       print("$i*$j=${i * j}");
+//     }
+//   }
+// }
