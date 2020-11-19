c44906340dca7ba50469c89dadc4dbd5480a6f50
Abae Akili <mohaabae@yahoo.com>
2020-11-19 11:30:01 +0300

    Lesson 8: ♻ For Loops

---
+++ for_loop.dart
@@ -0,0 +1,11 @@
+void main() {
+  List<String> names = ["Zahra", "Aboud", "Fatma"];
+
+  for (int i = 0; i < 2; i++) {
+    print("Hi");
+  }
+
+  for (int i = 0; i < names.length; i++) {
+    print(names[i]);
+  }
+}
