c6d60fdcdb234208aa368a6a16ef301dbc6489e1
Abae Akili <mohaabae@yahoo.com>
2020-11-19 09:16:35 +0300

    Lesson 5: 🛣 Conditions. If & else statemets.

---
+++ conditions.dart
@@ -0,0 +1,22 @@
+void main() {
+  bool birthday = true;
+
+  if (birthday) {
+    print("Happy Birthday");
+  } else {
+    print("Not Birthday");
+  }
+
+  int number = 2;
+
+  if (number != 2) ;
+  {
+    print("Not equal 2");
+  }
+
+  if (number >= 2) ;
+  {
+    print("Not bigger than 2");
+  }
+  
+}
