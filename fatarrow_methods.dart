7a1652549099ff64c238cff025f847b2a0c51a3b
Abae Akili <mohaabae@yahoo.com>
2020-11-19 10:03:49 +0300

    Lesson 6: 📨 Fat Arrow Methods

---
+++ fatarrow_methods.dart
@@ -0,0 +1,13 @@
+// Fat Arrow Methods
+
+// void main() {
+//   print(add(2, 3));
+// }
+
+void main()=> print(add(2, 3) + 4);
+
+// int add(int x, int y) {
+//   return (x + y);
+// }
+
+int add(int x, int y) => x + y;
