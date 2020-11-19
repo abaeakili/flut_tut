9c573abaef3acaf513150fd187e9e1c12ac38e55
Abae Akili <mohaabae@yahoo.com>
2020-11-19 09:54:50 +0300

    Lesson 6: 💎 Methods with return values

---
+++ method_parameters.dart
@@ -0,0 +1,15 @@
+// methods with return values
+
+void main() {
+  sayHi("Abae");
+  sayHi("Zahra");
+  add(2, 3);
+}
+
+void sayHi(String name) {
+  print("Hi $name");
+}
+
+void add(int x, int y) {
+  print(x + y);
+}
