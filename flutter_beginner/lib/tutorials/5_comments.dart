cdf4dfa889852c1cb4c6b1b8e6fb41bc56fe5fa2
Abae Akili <mohaabae@yahoo.com>
2020-11-20 14:31:28 +0300

    ➠ Adding Comments

---
+++ flutter_beginner/lib/tutorials/5_comments.dart
@@ -0,0 +1,18 @@
+// There are two types of comments. Multi-line comments and single-line comments
+// This is an example a single line comment 
+
+/* 
+this 
+Comment
+spans 
+several
+lines
+*/
+
+void main() {
+  var x = "10.6";
+
+  var y = num.parse(x);
+
+  print(y + 2);
+}
